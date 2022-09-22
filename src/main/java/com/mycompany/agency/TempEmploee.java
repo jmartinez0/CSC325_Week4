94bc587b060c34b1409d7269e0f482394eb46769
soblab <soblab@Whit209-20.it.farmingdale.edu>
2022-09-22 10:32:16 -0400

    TempEmploee addours and pay complete

--- src/main/java/com/mycompany/agency/TempEmploee.java
+++ src/main/java/com/mycompany/agency/TempEmploee.java
@@ -1,11 +1,8 @@
 package com.mycompany.agency;
 
+// TODO 1: Make TempEmploee a child of StaffEmploee
+public class TempEmploee extends StaffEmployee {
 
-
-    // TODO 1: Make TempEmploee a child of StaffEmploee
-
-public class TempEmploee 
-{
     private int hoursWorked;
 
     //-----------------------------------------------------------------
@@ -13,8 +10,8 @@ public class TempEmploee
     //  information.
     //-----------------------------------------------------------------
     public TempEmploee(String eName, String eAddress, String ePhone,
-            String socSecNumber, double rate)
-    {
+            String socSecNumber, double rate) {
+        
         super(eName, eAddress, ePhone, socSecNumber, rate);
 
         hoursWorked = 0;
@@ -24,24 +21,27 @@ public class TempEmploee
     // TODO2: Adds the specified number of hours to this employee's
     //  accumulated hours.
     //-----------------------------------------------------------------
-    public void addHours(int moreHours)
-    {
+    public void addHours(int moreHours) {
         
+        hoursWorked += moreHours;
+
     }
 
     //-----------------------------------------------------------------
     // TODO3: Computes and returns the pay for this hourly employee.
     //-----------------------------------------------------------------
-    public double pay()
-    {
-
+    public double pay() {
+        
+        return hoursWorked * pay();
+        
     }
 
     //-----------------------------------------------------------------
     // TODO4: Returns information about this hourly employee as a string.
     //-----------------------------------------------------------------
-    public String toString()
-    {
+    public String toString() {
+        
+        return ;
 
     }
 }
