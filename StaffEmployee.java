6aad40a588ac9226c4199a053eecfdef50de9258
jmartinez0 <89937578+jmartinez0@users.noreply.github.com>
2022-09-30 23:58:04 -0400

    Add files via upload

---
+++ StaffEmployee.java
@@ -0,0 +1,42 @@
+package com.mycompany.agency;
+
+
+    // TODO 1: Make StaffEmployee a child of StaffMember
+
+public class StaffEmployee extends StaffMember
+{
+    protected String socialSecurityNumber;
+    protected double payRate;
+
+    //-----------------------------------------------------------------
+    //  TODO 2: Sets up this staff employee with the specified
+    //  information. @todo Nur
+    //-----------------------------------------------------------------
+    public StaffEmployee(String eName, String eAddress, String ePhone,
+            String socSecNumber, double rate)
+    {
+        super(eName, eAddress, ePhone);
+
+        socialSecurityNumber = socSecNumber;
+        payRate = rate;
+    }
+
+    //-----------------------------------------------------------------
+    // TODO 3: complete the implementation of toString to return information 
+    //          about a staff employee as a string.
+    //-----------------------------------------------------------------
+    @Override
+    public String toString()
+    {
+        return name + " " + address + " " + phone + " " + socialSecurityNumber + " " + payRate;
+    }
+
+    //-----------------------------------------------------------------
+    // TODO 4:  Returns the pay rate for this staff employee.
+    //-----------------------------------------------------------------
+    @Override
+    public double pay()
+    {
+        return payRate;
+    }
+}
